e33baa73811a5a32c8d4c86fd13b03f19afe2b5d
Daniel Rey <reyvillanuevadaniel@gmail.com>
2024-09-11 13:14:46 +0800

    consume my own module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/greetings
+
+go 1.21.3
+
+require github.com/usernameNiD4niel/go-lang v0.0.0-20240911051057-4f2d3655c03c // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	golang "github.com/usernameNiD4niel/go-lang"
+)
+
+func main() {
+	message := golang.Hello("Daniel")
+
+	fmt.Println(message)
+}
